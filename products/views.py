2ec9c8c01d44411abf0e29d6e7ab9076f691ce58
aliciarawlings <lilyrawlings123@gmail.com>
2020-09-07 08:40:41 +0000

    created products.html file added path in urls

--- products/views.py
+++ products/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import Products
 
 
 def all_products(request):
-    """ Aview to show products """
-    return render(request, 'products/products.html')
+    """ A view to show products """
+ 
+    products = Products.objects.all()
+
+    context = {
+        'products': products,
+    }
+    return render(request, 'products/products.html', context)
