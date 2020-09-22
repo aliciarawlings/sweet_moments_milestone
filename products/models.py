4009cbf2471be557787b33b27c197273976e663a
aliciarawlings <lilyrawlings123@gmail.com>
2020-09-22 14:14:43 +0000

    quantity field changed in products models

--- products/models.py
+++ products/models.py
@@ -4,8 +4,7 @@ from django.db import models
 class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
-
-    name = models.CharField(max_length=250)
+    
     flavour = models.CharField(max_length=250)
 
     def __str__(self):
@@ -26,7 +25,7 @@ class Products(models.Model):
     quantity = models.DecimalField(choices=QUANTITY_CHOICES, default=0.5, decimal_places= 2, max_digits=6)
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=250, null=True)
-    name = models.CharField(max_length=250)
+    name = models.CharField(max_length=250,null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
