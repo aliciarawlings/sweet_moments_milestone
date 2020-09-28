7466b583bbc5eab5443520deaf7bbe4148d27a1a
aliciarawlings <lilyrawlings123@gmail.com>
2020-09-28 00:58:47 +0000

    styling added to cart page and typo in stripe_js file corrected to enable payments

--- static/script/stripe_elements.js
+++ static/script/stripe_elements.js
@@ -1,9 +1,7 @@
-var stripePublicKey = $('#id_stripe_public_key')
-var clientSecret = $('#client_secret')
-var stripe = Stripe('stripePublicKey');
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1,-1);
+var stripe = Stripe(stripePublicKey);
 var elements= stripe.elements();
-var card = elements.create('card');
-card.mount('#card-element');
 
 var style = {
     base: {
@@ -20,28 +18,46 @@ var style = {
         iconColor: '#dc3545'
     }
 };
+var card = elements.create('card',{style: style});
+card.mount('#card-element');
+
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
 
 var checkoutForm = document.getElementById('checkout_form');
 
 checkoutForm.addEventListener('submit', function(ev) {
-    ev.preventDefault();
-    card.update({'disabled': true});
+    ev.preventDefault(); //prevents default action (POST)
+    card.update({'disabled': true}); //prevent multiple submission
     $('#submit-button').attr('disabled', true);
-   stripe.confirmCardPayment(clientSecret, {
+    stripe.confirmCardPayment(clientSecret, { //sends info to stripe
        payment_method:{
            card:card,
        }
-   }).then(function(result){
-       if (result.error){
-           alert("{result.error..message}") ;
-        card.update({'disabled': false});
+    }).then(function(result){
+       if (result.error) {
+           alert("error");
+           console.log(result.error)
+        card.update({'disabled': false}); //renable to allow user to fix it
         $('#submit-button').attr('disabled', false);
-    }else{
-        if (result.paymentIntent.status === "succeeded"){
+    } else {
+        if (result.paymentIntent.status === 'succeeded'){
             checkoutForm.submit();
         }
     }
-   })
+   });
+});
 
-})
 
