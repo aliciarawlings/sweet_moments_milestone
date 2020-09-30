e1d41639970f42cf84748fad48000ee0f67f87f7
aliciarawlings <lilyrawlings123@gmail.com>
2020-09-30 03:05:38 +0000

    database key comprimised from accidentaly push to git. new key created for security purposes

--- sweet_moments/settings.py
+++ sweet_moments/settings.py
@@ -149,9 +149,6 @@ WSGI_APPLICATION = 'sweet_moments.wsgi.application'
     #}
 #}
 
-DATABASES = {
-    'default':dj_database_url.parse('postgres://oqsfjnbrlckpfm:565ec29d775d2ac1905299d2c5f2866c86b2381d0d3b2c728371f625bdf5d9a5@ec2-3-248-4-172.eu-west-1.compute.amazonaws.com:5432/db5kvjv55rb6gu')
-}
 
 
 # Password validation
